43548951d8246bcf34ba09898313b82b83c8a7ac
Supriyosaha1 <supriyosaha5120@gmail.com>
2023-11-15 03:32:53 +0530

    Eliminating Found Flag and used while-else Construction

--- power.py
+++ power.py
@@ -1,13 +1,11 @@
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
-found = False
+
 i = 0
-while not found and i < len(L):
+while i <= len(L):
     if 2 ** X == L[i]:
-        found = True
-    else:
-        i = i+1
-if found:
-    print('at index', i)
+        print("At Index",i)
+    i = i+1
+
 else:
     print(X, 'not found')
