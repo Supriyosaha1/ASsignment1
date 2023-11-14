11372980b30924f481b2ea774f418c9d92476fd6
Supriyosaha1 <supriyosaha5120@gmail.com>
2023-11-15 05:14:35 +0530

    Moving 2**x out of the loops

--- power.py
+++ power.py
@@ -1,12 +1,12 @@
 L = []
 X = 5
 
-#target_value=2**X
+target_value=2**X
 
 for i in range(10):
     L.append(2**i)
     
-    if 2**X==L[i]:
+    if target_value==L[i]:
         print("At index:",i)
         break
 else:
